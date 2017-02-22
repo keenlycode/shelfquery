6d103ad7c9d2592a0c39def9a175466cb803bef3
Um Nontasuwan <nitipit@gmail.com>
2017-02-22 22:59:29 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='shelfquery',
+    version='0.1.0',
+    description='ShelfDB query client for using with `shelfdb` server',
+    url='https://github.com/nitipit/shelfquery',
+    author='Nitipit Nontasuwan',
+    author_email='nitipit@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='shelfdb query client',
+    packages=find_packages(),
+    install_requires=['dill',],
+)
