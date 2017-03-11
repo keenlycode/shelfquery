7d2d524fc697ff8d2e44f2a5b13130a70d3a688b
Um Nontasuwan <nitipit@gmail.com>
2017-03-11 10:50:03 +0700

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shelfquery',
-    version='0.1.0',
+    version='0.1.1',
     description='ShelfDB query client for using with `shelfdb` server',
     url='https://github.com/nitipit/shelfquery',
     author='Nitipit Nontasuwan',
