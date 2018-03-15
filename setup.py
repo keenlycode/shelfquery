286f5c64635bf707b4ec2065bacd79ff69c8907b
Nitipit Nontasuwan <nitipit@gmail.com>
2018-03-16 02:59:17 +0700

    up version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='shelfquery',
-    version='0.1.1',
+    version='1.0.0',
     description='ShelfDB query client for using with `shelfdb` server',
     url='https://github.com/nitipit/shelfquery',
     author='Nitipit Nontasuwan',
     author_email='nitipit@gmail.com',
     license='MIT',
     classifiers=[
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='shelfdb query client',
     packages=find_packages(),
